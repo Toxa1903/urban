fef2cbf9f7075d036e125196d88d4d90cde3fbd6
Toxa1903 <97594540+Toxa1903@users.noreply.github.com>
2024-11-16 12:33:14 +0500

    Create module_6_hard.py

---
+++ module_6_hard.py
@@ -0,0 +1,81 @@
+class Figure:
+    sides_count = 0
+
+    def __init__(self, color, *sides):
+        self.__color = list(color) if self.__is_valid_color(*color) else [0, 0, 0]
+        self.sides = list(sides) if self.__is_valid_sides(*sides) else [1] * self.sides_count
+        self.filled = False
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def __is_valid_color(self, r, g, b):
+        return all(isinstance(x, int) and 0 <= x <= 255 for x in (r, g, b))
+
+    def __is_valid_sides(self, *sides):
+        return len(sides) == self.sides_count and all(isinstance(side, int) and side > 0 for side in sides)
+
+    def get_sides(self):
+        return self.sides
+
+    def __len__(self):
+        return sum(self.sides)
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.sides = list(new_sides)
+
+
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__radius = self.sides[0] / (2 * 3.141592653589793)
+
+    def get_square(self):
+        return 3.141592653589793 * (self.__radius ** 2)
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def get_square(self):
+        a, b, c = self.sides
+        s = (a + b + c) / 2
+        return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, *sides):
+        if len(sides) == 1:
+            sides = [sides[0]] * self.sides_count
+        super().__init__(color, *sides)
+
+    def get_volume(self):
+        side_length = self.sides[0]
+        return side_length ** 3
+
+
+circle1 = Circle((200, 200, 100), 10)
+cube1 = Cube((222, 35, 130), 6)
+
+circle1.set_color(55, 66, 77)  
+print(circle1.get_color())
+cube1.set_color(300, 70, 15)
+print(cube1.get_color())
+
+cube1.set_sides(5, 3, 12, 4, 5)
+print(cube1.get_sides())
+circle1.set_sides(15)
+print(circle1.get_sides())
+
+print(len(circle1))
+
+print(cube1.get_volume())
